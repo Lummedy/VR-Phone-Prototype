f9bf7837dd9777f9ed6a1352d10decc98873abcb
Lummedy <mark-de-bruin@hotmail.com>
2023-08-01 03:17:55 +0200

    Initial commit

---
+++ Assets/Scripts/GyroRotate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+public class GyroRotate : MonoBehaviour
+{
+
+    private Gyroscope gyro;
+
+    void Start()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+        }
+        else
+        {
+            Debug.Log("Phone doesen't support");
+        }
+    }
+
+    void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.transform.Rotate(-Input.gyro.rotationRateUnbiased.x * 2, -Input.gyro.rotationRateUnbiased.y * 2, 0);
+    }
+
+    
+}
---
+++ Assets/Scripts/SelfDestroy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestroy : MonoBehaviour
+{
+    [Header("Time")]
+    public float time;
+ 
+
+    
+    void Update()
+    {
+        time = time - 1 * Time.deltaTime;
+
+        if (time <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
